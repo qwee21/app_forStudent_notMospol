ff37da4257c726d1593df6d74860b700ac56b2d3
Ilya Barashkov <rete234@mail.ru>
2024-05-31 21:41:08 +0300

    added setting.py

--- notmospol/main/urls.py
+++ notmospol/main/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 
+from main import views
 
+app_name = 'main'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', views.index, name='index'),
+    # path('specialties/', views.specialties, name='specialties'),
 ]
--- notmospol/main/views.py
+++ notmospol/main/views.py
@@ -5,11 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Notmospol - сайт по поддержке студентов'
+        'title': 'NotMospol',
+        'content': 'Notmospol- сайт по поддержке студентов'
     }
 
     return render(request,'main/index.html', context)
 
-def specialties(request):
-    return HttpResponse("Specialties page")
